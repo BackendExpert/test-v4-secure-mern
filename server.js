cb5d53ab68433cf5a92ecc7695caa84eba45c8df
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-08-26 07:26:49 +0530

    Update

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const secureMern = require("secure-mern");
+const path = require('path')
+
+require("dotenv").config();
+
+// const admincontrolroute = require('./routes/admincotrolroute')
+
+const app = express();
+
+secureMern(app);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// app.use('/admin', admincontrolroute)
+
+app.get('/', (req, res) => {
+    res.send(`Server running on port ${process.env.PORT}`);
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
